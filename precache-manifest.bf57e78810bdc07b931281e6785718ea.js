5766b00687d125aee38b313f8b5755a4a785d2dd
Indra Mohan <indrathota1993@gmail.com>
2019-10-05 12:54:09 +0530

    Updates

---
+++ precache-manifest.bf57e78810bdc07b931281e6785718ea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82217c906da50a93749010195cdc8836",
+    "url": "/index.html"
+  },
+  {
+    "revision": "44341bed8017d39b01bc",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "44341bed8017d39b01bc",
+    "url": "/static/js/2.fec3ba5f.chunk.js"
+  },
+  {
+    "revision": "957147bdff48db5cce66",
+    "url": "/static/js/main.169d6534.chunk.js"
+  },
+  {
+    "revision": "94e83cc570b3f48809d2",
+    "url": "/static/js/runtime-main.82a6b6f6.js"
+  }
+]);
